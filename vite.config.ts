a2af9666ed98c40c1118c16ad8d172f622f13d62
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2026-04-05 16:46:44 +0100

    Add config related files

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  css: {
+    postcss: {
+      plugins: [tailwind, autoprefixer]
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          mui: ['@mui/icons-material', '@mui/joy']
+        }
+      }
+    }
+  }
+});
