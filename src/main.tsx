311ab1a5bf3c360ca891729dcce3cf4ec7eec9c0
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2026-04-05 19:36:54 +0100

    added fontsource/inter in main component

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
