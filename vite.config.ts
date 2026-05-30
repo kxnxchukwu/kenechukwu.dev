74f3e92bda41ca953f74fb4f1fe7bed5ba7943f3
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2026-05-30 11:08:47 +0100

    Add test to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tailwind from '@tailwindcss/postcss';
@@ -7,6 +7,15 @@ import autoprefixer from 'autoprefixer';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.ts',
+    include: ['**/__tests__/**/*.{ts,tsx}'],
+    coverage: {
+      exclude: ['**/index.ts', '**/*.css', '**/main.tsx']
+    }
+  },
   css: {
     postcss: {
       plugins: [tailwind, autoprefixer]
